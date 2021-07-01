817cfc0fca89c3f9d651334b64ccbc9f3c795a90
Maarten de Klerk <M.F.deKlerk-2@umcutrecht.nl>
2021-07-01 12:48:06 +0200

    feat: add model analysis notebook

--- utils/concrete/__init__.py
+++ utils/concrete/__init__.py
@@ -1,2 +1,2 @@
+from .concrete_select import ConcreteSelect, decoder_1l, decoder_2l, decoder_3l
 from .feature_selector import ConcreteAutoencoderFeatureSelector
-from .concrete_select import decoder_1l, decoder_2l, decoder_3l

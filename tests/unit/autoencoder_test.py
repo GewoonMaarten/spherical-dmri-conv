fcad649dc2ae4193d42ec21b89d6e5926c12453a
Maarten de Klerk <maarten@deklerk.dev>
2021-09-03 14:18:16 +0200

    test: add new decoder test case

--- tests/unit/autoencoder_test.py
+++ tests/unit/autoencoder_test.py
@@ -4,6 +4,12 @@ from autoencoder.concrete_autoencoder import Decoder
 test_data = [
     (500, 1344, 1, ["linear_0", "relu_0", "linear_1", "sigmoid_1"]),
     (3000, 1344, 1, ["linear_0", "relu_0", "linear_1", "sigmoid_1"]),
+    (
+        500,
+        1344,
+        2,
+        ["linear_0", "relu_0", "linear_1", "relu_1", "linear_2", "sigmoid_2"],
+    ),
 ]
 
 
@@ -14,6 +20,7 @@ def test_decoder_network(input_size, output_size, n_hidden_layers, expected_laye
     for i, module in enumerate(
         Decoder(input_size, output_size, n_hidden_layers).decoder.named_children()
     ):
+        print(module)
         assert module[0] == expected_layers[i]
         if i == len(expected_layers):
             assert module[1].out_features == output_size

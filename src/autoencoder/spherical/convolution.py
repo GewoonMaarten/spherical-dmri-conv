1fe364fc46f4280d03647e26e6dfd5d4c86b7385
Maarten <maarten@deklerk.dev>
2021-12-10 13:11:09 +0100

    feat: add profiler param to spherical convolutions

--- src/autoencoder/spherical/convolution.py
+++ src/autoencoder/spherical/convolution.py
@@ -1,5 +1,6 @@
 import torch
 import numpy as np
+from pytorch_lightning.profiler import PassThroughProfiler
 
 
 def quadratic_non_linearity(rh, L_in, L_out, CG_r, CG_l, symmetric: int = 2):
@@ -35,7 +36,18 @@ def quadratic_non_linearity(rh, L_in, L_out, CG_r, CG_l, symmetric: int = 2):
 
 class S2Convolution(torch.nn.Module):
     def __init__(
-        self, ti_n, te_n, l_in, l_out, b_in, b_out, cg_r, cg_l, *, symmetric=2
+        self,
+        ti_n,
+        te_n,
+        l_in,
+        l_out,
+        b_in,
+        b_out,
+        cg_r,
+        cg_l,
+        *,
+        symmetric=2,
+        profiler=None,
     ):
         """Convolution between spherical signals and kernels in spectral domain."""
         super(S2Convolution, self).__init__()
@@ -45,6 +57,8 @@ class S2Convolution(torch.nn.Module):
         self.symmetric = symmetric
         self.cg_r = cg_r
         self.cg_l = cg_l
+
+        self.profiler = profiler or PassThroughProfiler()
 
         self.weights = dict()
         for l in range(0, self.l_in + 1, self.symmetric):
@@ -60,39 +74,54 @@ class S2Convolution(torch.nn.Module):
         self.bias = torch.nn.Parameter(torch.zeros(ti_n, te_n, 1, b_out, 1, 1))
 
     def forward(self, x):
-        # convolution
-        rh = dict()
-        for l in range(0, self.l_in + 1, self.symmetric):
-            rh[l] = torch.einsum("nabil, abiok->abnolk", x[l], self.weights[l])
-            rh[l] += self.bias if l == 0 else 0
+        with self.profiler.profile("S2Convolution"):
+            # convolution
+            rh = dict()
+            for l in range(0, self.l_in + 1, self.symmetric):
+                rh[l] = torch.einsum("nabil, abiok->abnolk", x[l], self.weights[l])
+                rh[l] += self.bias if l == 0 else 0
 
-        # activation function
-        rh_n = quadratic_non_linearity(
-            rh,
-            self.l_out,
-            self.l_in,
-            self.cg_r,
-            self.cg_l,
-            symmetric=self.symmetric,
-        )
-
-        # feature extraction
-        for l in range(0, self.l_out + 1, self.symmetric):
-            n_l = 8 * (np.pi ** 2) / (2 * l + 1)
-            feats_l = torch.flatten(
-                torch.sum(torch.pow(rh_n[l], 2), (5, 4)), start_dim=2
+            # activation function
+            rh_n = quadratic_non_linearity(
+                rh,
+                self.l_out,
+                self.l_in,
+                self.cg_r,
+                self.cg_l,
+                symmetric=self.symmetric,
             )
-            if l == 0:
-                feats = n_l * feats_l
-            else:
-                feats = torch.cat((feats, n_l * feats_l), dim=2)
+
+            # feature extraction
+            for l in range(0, self.l_out + 1, self.symmetric):
+                n_l = 8 * (np.pi ** 2) / (2 * l + 1)
+
+                rh_n_l_t = torch.transpose(rh_n[l], 2, 0)
+                rh_n_l_p = torch.pow(rh_n_l_t, 2)
+                rh_n_l_s = torch.sum(rh_n_l_p, (5, 4))
+                feats_l = torch.flatten(rh_n_l_s, start_dim=1)
+
+                if l == 0:
+                    feats = n_l * feats_l
+                else:
+                    feats = torch.cat((feats, n_l * feats_l), dim=1)
 
         return rh_n, feats
 
 
 class SO3Convolution(torch.nn.Module):
     def __init__(
-        self, ti_n, te_n, l_in, l_out, b_in, b_out, cg_r, cg_l, *, symmetric=2
+        self,
+        ti_n,
+        te_n,
+        l_in,
+        l_out,
+        b_in,
+        b_out,
+        cg_r,
+        cg_l,
+        *,
+        symmetric=2,
+        profiler=None,
     ):
         """Convolution between SO(3) signals and kernels in spectral domain."""
         super(SO3Convolution, self).__init__()
@@ -102,6 +131,8 @@ class SO3Convolution(torch.nn.Module):
         self.symmetric = symmetric
         self.cg_r = cg_r
         self.cg_l = cg_l
+
+        self.profiler = profiler or PassThroughProfiler()
 
         self.weights = dict()
         for l in range(0, self.l_in + 1, self.symmetric):
@@ -115,33 +146,37 @@ class SO3Convolution(torch.nn.Module):
         self.bias = torch.nn.Parameter(torch.zeros(ti_n, te_n, 1, b_out, 1, 1))
 
     def forward(self, x):
-        # convolution
-        rh = dict()
-        for l in range(0, self.l_in + 1, self.symmetric):
-            rh[l] = (2 * l + 1) * torch.einsum(
-                "abnilk, abiokj->abnolj", x[l], self.weights[l]
-            )
-            rh[l] += self.bias if l == 0 else 0
+        with self.profiler.profile("SO3Convolution"):
+            # convolution
+            rh = dict()
+            for l in range(0, self.l_in + 1, self.symmetric):
+                rh[l] = (2 * l + 1) * torch.einsum(
+                    "abnilk, abiokj->abnolj", x[l], self.weights[l]
+                )
+                rh[l] += self.bias if l == 0 else 0
 
-        # activation function
-        rh_n = quadratic_non_linearity(
-            rh,
-            self.l_out,
-            self.l_in,
-            self.cg_r,
-            self.cg_l,
-            symmetric=self.symmetric,
-        )
-
-        # feature extraction
-        for l in range(0, self.l_out + 1, self.symmetric):
-            n_l = 8 * (np.pi ** 2) / (2 * l + 1)
-            feats_l = torch.flatten(
-                torch.sum(torch.pow(rh_n[l], 2), (5, 4)), start_dim=2
+            # activation function
+            rh_n = quadratic_non_linearity(
+                rh,
+                self.l_out,
+                self.l_in,
+                self.cg_r,
+                self.cg_l,
+                symmetric=self.symmetric,
             )
-            if l == 0:
-                feats = n_l * feats_l
-            else:
-                feats = torch.cat((feats, n_l * feats_l), dim=2)
+
+            # feature extraction
+            for l in range(0, self.l_out + 1, self.symmetric):
+                n_l = 8 * (np.pi ** 2) / (2 * l + 1)
+
+                rh_n_l_t = torch.transpose(rh_n[l], 2, 0)
+                rh_n_l_p = torch.pow(rh_n_l_t, 2)
+                rh_n_l_s = torch.sum(rh_n_l_p, (5, 4))
+                feats_l = torch.flatten(rh_n_l_s, start_dim=1)
+
+                if l == 0:
+                    feats = n_l * feats_l
+                else:
+                    feats = torch.cat((feats, n_l * feats_l), dim=1)
 
         return rh_n, feats

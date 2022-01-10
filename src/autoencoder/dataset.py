26ad2dda797e21425ed0f117dccc8874455994c7
Maarten <maarten@deklerk.dev>
2022-01-10 16:04:44 +0100

    refactor: turn MRIMemorySHDataset into a transform function
    this function can be used as an argument for the other datasets

--- src/autoencoder/dataset.py
+++ src/autoencoder/dataset.py
@@ -1,12 +1,11 @@
+import copy
 import itertools
-import os
 from argparse import ArgumentParser
 from pathlib import Path
 from typing import Optional
 
 import h5py
 import numpy as np
-import psutil
 import pytorch_lightning as pl
 import torch
 from torch.utils.data import DataLoader, Dataset
@@ -16,63 +15,18 @@ from autoencoder.logger import logger
 from autoencoder.spherical.harmonics import gram_schmidt_sh_inv, sh_basis_real
 
 
-class MRIMemorySHDataset(Dataset):
+class SphericalTransformer(object):
     def __init__(
-        self,
-        data_file_path: Path,
-        subject_list: list[int],
-        exclude: Optional[list[int]] = None,
-        include: Optional[list[int]] = None,
-        l_max: int = 0,
-        symmetric: bool = True,
-        gram_schmidt_n_iters: int = 1000,
-    ):
-        """Create a dataset from the selected subjects in the subject list with matching spherical harmonics.
-
-        Args:
-            data_file_path (Path): Data h5 file path.
-            subject_list (list[int]): ist of all the subjects to include.
-        """
-        self._data_file_path = data_file_path
-        self._subject_list = subject_list
+        self, l_max: int = 0, symmetric: bool = True, inversion_n_iters: int = 1000
+    ) -> None:
         self._l_max = l_max
         self._symmetric = 2 if symmetric else 1
-        self._gram_schmidt_n_iters = gram_schmidt_n_iters
+        self._inversion_n_iters = inversion_n_iters
 
-        assert (
-            exclude is None or include is None
-        ), "Only specify include or exclude, not both."
+    def __call__(self, **kwargs):
+        data = kwargs["data"]
+        scheme = kwargs["scheme"]
 
-        # load the data in memory. The total file is *only* 3.1GB so it should be
-        # doable on most systems. Lets check anyway...
-        file_size = os.path.getsize(data_file_path)
-        available_memory = psutil.virtual_memory().available
-
-        assert (
-            available_memory - file_size >= 0
-        ), f"Data file requires {file_size:,} bytes of memory but {available_memory:,} was available"
-
-        with h5py.File(data_file_path, "r") as archive:
-            scheme = archive.get("scheme")[()]
-            indexes = archive.get("index")[()]
-
-            # indexes of the data we want to load
-            (selection, *_) = np.where(np.isin(indexes, subject_list))
-
-            self.data = archive.get("data")[selection]
-
-            if include is not None:
-                scheme = scheme[include]
-                self.data_filtered = self.data[:, include]
-            elif exclude is not None:
-                scheme = np.delete(scheme, exclude, axis=0)
-                self.data_filtered = np.delete(self.data, exclude, axis=1)
-
-            self.sh_coefficients = self._load_sh_coefficients(
-                self.data_filtered, scheme
-            )
-
-    def _load_sh_coefficients(self, data, scheme) -> list[np.ndarray]:
         b_s = np.unique(scheme[:, 3])  # 5 unique values
         ti_s = np.unique(scheme[:, 4])  # 28 unique values
         te_s = np.unique(scheme[:, 5])  # 3 unique values
@@ -84,8 +38,8 @@ class MRIMemorySHDataset(Dataset):
         prev_b = b_s[0]
 
         # Fit the spherical harmonics on the gradients.
-        y = sh_basis_real(torch.from_numpy(scheme[:, :3]), self._l_max)
-        y_inv = gram_schmidt_sh_inv(y, self._l_max, n_iters=self._gram_schmidt_n_iters)
+        y = sh_basis_real(torch.from_numpy(scheme[:, :3]), self._l_max).to(data.device)
+        y_inv = gram_schmidt_sh_inv(y, self._l_max, n_iters=self._inversion_n_iters)
 
         sh_coefficients_b_idx = dict()
         sh_coefficients = dict()
@@ -93,7 +47,9 @@ class MRIMemorySHDataset(Dataset):
         for l in range(0, self._l_max + 1, self._symmetric):
             o = 2 * l + 1
             sh_coefficients_b_idx[l] = 0
-            sh_coefficients[l] = torch.zeros((ti_n, te_n, data.shape[0], b_n, o))
+            sh_coefficients[l] = torch.zeros(
+                (data.shape[0], ti_n, te_n, b_n, o), device=data.device
+            )
             l_sizes[l] = o
 
         for (ti_idx, ti), (te_idx, te), b in itertools.product(
@@ -113,7 +69,7 @@ class MRIMemorySHDataset(Dataset):
             if not np.any(filter_scheme):
                 continue
 
-            data_filtered = torch.from_numpy(data[:, filter_scheme])
+            data_filtered = data[:, filter_scheme]
 
             # Get the maximum l where the amount of coefficients is smaller or equal to the amount of voxels.
             l = [l for l, o in l_sizes.items() if o <= data_filtered.shape[1]][-1]
@@ -128,32 +84,13 @@ class MRIMemorySHDataset(Dataset):
                 o = 2 * l + 1
 
                 sh_coefficients[l][
-                    ti_idx, te_idx, :, sh_coefficients_b_idx[l]
+                    :, ti_idx, te_idx, sh_coefficients_b_idx[l]
                 ] = sh_coefficient[:, torch.arange(s, s + o)]
 
                 s += o
                 sh_coefficients_b_idx[l] += 1
 
         return sh_coefficients
-
-    def __len__(self):
-        """Denotes the total number of samples"""
-        return self.sh_coefficients[0].shape[2]
-
-    def __getitem__(self, index):
-        """Generates one sample of data"""
-        return {
-            "data": {k: v[:, :, index] for (k, v) in self.sh_coefficients.items()},
-            "target": self.data[index],
-        }
-
-    def __getstate__(self):
-        """Return state values to be pickled."""
-        return None
-
-    def __setstate__(self, state):
-        """Restore state from the unpickled state values."""
-        pass
 
 
 class MRIMemoryDataset(Dataset):
@@ -163,7 +100,8 @@ class MRIMemoryDataset(Dataset):
         subject_list: list[int],
         exclude: Optional[list[int]] = None,
         include: Optional[list[int]] = None,
-        do_store_in_gpu: bool = True,
+        do_preload_in_gpu: bool = True,
+        transform=None,
     ):
         """Create a dataset from the selected subjects in the subject list
 
@@ -172,38 +110,45 @@ class MRIMemoryDataset(Dataset):
             subject_list (np.ndarray): ist of all the subjects to include.
             exclude (list[int], optional): list of features to exclude from training. Cannot be used together with include. Defaults to None.
             include (list[int], optional): list of features to only include for training. Cannot be used together with exclude. Defaults to None.
-            do_store_in_gpu (bool, optional): store all data in GPU memory. Defaults to True.
+            do_preload_in_gpu (bool, optional): preload all data in GPU memory, instead of for each batch. Defaults to True.
         """
 
         assert (
             exclude is None or include is None
         ), "Only specify include or exclude, not both."
 
-        self._exclude = exclude
-        self._include = include
-
         with h5py.File(data_file_path, "r") as archive:
+            scheme = archive.get("scheme")[()]
             indexes = archive.get("index")[()]
             # indexes of the data we want to load
             (selection, *_) = np.where(np.isin(indexes, subject_list))
-            self.data = archive.get("data")[selection]
+            self.target = archive.get("data")[selection]
+            self.sample = copy.deepcopy(self.target)
 
-        if do_store_in_gpu:
-            self.data = torch.from_numpy(self.data).to("cuda")
+        if include is not None:
+            self.sample = self.sample[:, include]
+            scheme = scheme[include]
+        elif exclude is not None:
+            self.sample = np.delete(self.sample, exclude)
+            scheme = np.delete(scheme, exclude)
+
+        self.target = torch.from_numpy(self.target)
+        self.sample = torch.from_numpy(self.sample)
+
+        if do_preload_in_gpu:
+            self.target = self.target.to("cuda")
+            self.sample = self.sample.to("cuda")
+
+        if transform:
+            self.sample = transform(data=self.sample, scheme=scheme)
 
     def __len__(self):
         """Denotes the total number of samples"""
-        return len(self.data)
+        return len(self.target)
 
     def __getitem__(self, index):
         """Generates one sample of data"""
-        data = self.data[index]
-        if self._include is not None:
-            return data, data[self._include]
-        elif self._exclude is not None:
-            return data, np.delete(data, self._exclude)
-        else:
-            return data
+        return self.target[index], self.sample[index]
 
     def __getstate__(self):
         """Return state values to be pickled."""
@@ -221,6 +166,7 @@ class MRIDataset(Dataset):
         subject_list: list[int],
         exclude: Optional[list[int]] = None,
         include: Optional[list[int]] = None,
+        transform=None,
     ):
         """Create a dataset from the selected subjects in the subject list
 
@@ -242,9 +188,12 @@ class MRIDataset(Dataset):
         self._exclude = exclude
         self._include = include
         self._data_file_path = data_file_path
+        self._transform = transform
 
         with h5py.File(self.data_file_path, "r") as archive:
+            self._scheme = archive.get("scheme")[()]
             indexes = archive.get("index")[()]
+
         # indexes of the data we want to load
         (self.selection, *_) = np.where(np.isin(indexes, subject_list))
 
@@ -256,6 +205,9 @@ class MRIDataset(Dataset):
         """Generates one sample of data"""
         with h5py.File(self.data_file_path, "r") as archive:
             data = archive.get("data")[self.selection[index]]
+
+        if self._transform:
+            self.sample = self._transform(data=self.sample, scheme=self._scheme)
 
         if self._include is not None:
             return data, data[self._include]

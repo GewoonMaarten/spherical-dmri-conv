fd5f47b69ae89acb8680587090d076a42c200aeb
Maarten <maarten@deklerk.dev>
2022-01-30 21:31:24 +0100

    chore: add more artifacts

--- trainer.py
+++ trainer.py
@@ -3,8 +3,9 @@ import os
 import mlflow
 import numpy as np
 import pytorch_lightning as pl
+import torch
 from pytorch_lightning.utilities.cli import DATAMODULE_REGISTRY, MODEL_REGISTRY, LightningCLI
-import copy
+
 import autoencoder.datasets
 import autoencoder.models
 from autoencoder.logger import set_log_level
@@ -30,9 +31,16 @@ if __name__ == "__main__":
     cli.trainer.fit(model=cli.model, datamodule=cli.datamodule)
     cli.trainer.test(ckpt_path="best", datamodule=cli.datamodule)
 
+    scripted_pytorch_model = torch.jit.script(cli.model)
+    mlflow.pytorch.log_model(scripted_pytorch_model, "scripted_model")
+
     if experiment_name == "ConcreteAutoencoder":
         latent_features_file_path = "logs/latent_features.txt"
         np.savetxt(
             latent_features_file_path, np.array(cli.model.encoder.latent_features, dtype=int), fmt="%d",
         )
+        mlflow.log_artifact(latent_features_file_path)
+    else:
+        latent_features_file_path = "logs/latent_features.txt"
+        np.savetxt(latent_features_file_path, cli.datamodule.val_dataset._selected_parameters, fmt="%d")
         mlflow.log_artifact(latent_features_file_path)
